a7c0945f4e972d1f6fa02c1f9d3cdec665f01759
Sergio Sanchez <trukise@gmail.com>
2016-01-29 23:43:03 +0100

    Add pacientes model to Admin site

--- logopedasur/logopedasur/pacientes/admin.py
+++ logopedasur/logopedasur/pacientes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from logopedasur.pacientes.models import Pacientes
+
+# Register your models here.
+class PacientesAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Pacientes, PacientesAdmin)
