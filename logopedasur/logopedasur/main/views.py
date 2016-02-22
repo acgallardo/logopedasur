262693c4475da819008085a6cfa4fbcb7e99a181
Sergio Sanchez <trukise@gmail.com>
2016-02-22 17:41:10 +0100

    Index template is refactored by including blocks section with include tags respective. We create some html to include on base.html, inc.menu_footer_buttons.html and inc.menu_lateral.html

--- logopedasur/logopedasur/main/views.py
+++ logopedasur/logopedasur/main/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render, render_to_response
 
 # Create your views here.
 def index(request):
-    return render_to_response("main/index.html", {}, context_instance=RequestContext(request))
+    return render_to_response("main/index.html", {"nombre":"trukise"}, context_instance=RequestContext(request))
