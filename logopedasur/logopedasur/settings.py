76fdaa659c908615f96d0b0e8f872dbbaeef13d5
Sergio Sanchez <trukise@gmail.com>
2016-02-23 13:15:12 +0100

    refactoring on templates, dividing html and django template on blocks

--- logopedasur/logopedasur/settings.py
+++ logopedasur/logopedasur/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'logopedasur.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'logopedasur','templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'logopedasur', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- logopedasur/logopedasur/urls.py
+++ logopedasur/logopedasur/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from logopedasur.main  import views
+from logopedasur.main import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
