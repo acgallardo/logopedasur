67e3056a77e58829162c7c50fd1b337b12757982
Sergio Sanchez <trukise@gmail.com>
2016-03-02 01:18:30 +0100

    Create Session models.py

--- logopedasur/logopedasur/sesiones/models.py
+++ logopedasur/logopedasur/sesiones/models.py
@@ -1,5 +1,30 @@
-from __future__ import unicode_literals
-
+from datetime import date
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+
+from logopedasur.terapeutas.models import Terapeuta
+from logopedasur.pacientes.models import Paciente, Tutor
+
 
 # Create your models here.
+
+
+@python_2_unicode_compatible
+class Sesion(models.Model):
+    ''' Model for table Sessions''''
+
+    class Meta:
+        verbose_name = _('sesion')
+        verbose_name_plural = _('sesiones')
+
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    terapeutas = models.ManyToManyField(Terapeuta)
+    fecha_sesion = models.DateField(_('Fecha de sesion'))
+    hora_inicio = models.TimeField(_('Hora de inicio'))
+    hora_fin = models.TimeField(_('Hora de finalizacion'))
+    observaciones = models.TextField(_('observaciones'), blank=True, null=True)
+
+
+    def __str__(self):
+        return self.paciente.nombre + self.paciente.nombre + self.fecha_sesion + self.hora_inicio
