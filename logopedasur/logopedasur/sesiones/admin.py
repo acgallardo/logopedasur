2e76b29bd325a8306e5969511ac3d10bc7439e43
Sergio Sanchez <trukise@gmail.com>
2016-03-02 17:32:56 +0100

    Some changes in sessions model, we create a table to specified types of sessions, before we had CHOICES OPTIONS inside session table

--- logopedasur/logopedasur/sesiones/admin.py
+++ logopedasur/logopedasur/sesiones/admin.py
@@ -1,15 +1,27 @@
 from django.contrib import admin
 
-from logopedasur.sesiones.models import Sesion
+from logopedasur.sesiones.models import Sesion, Sesiontipo
 
 # Register your models here.
+class SesionestipoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'facturable', 'coste')
+    search_fields = ('nombre', 'coste',)
+    list_filter = ('nombre', 'coste',)
+
 class SesionesAdmin(admin.ModelAdmin):
-    list_display = ('paciente_relacionado','fecha_sesion', 'hora_inicio', 'hora_fin')
+    list_display = ('paciente_relacionado', 'tipo_sesion', 'fecha_sesion', 'hora_inicio', 'hora_fin')
     search_fields = ('fecha_sesion',)
     list_filter = ('fecha_sesion',)
 
     def paciente_relacionado(self, obj):
         return obj.paciente.nombre + ' ' + obj.paciente.apellidos
 
+    def tipo_sesion(self, obj):
+        return obj.tipo.nombre
+
+
     paciente_relacionado.short_description = 'Paciente'
+    tipo_sesion.short_description = 'Tipo'
+
 admin.site.register(Sesion, SesionesAdmin)
+admin.site.register(Sesiontipo, SesionestipoAdmin)
--- logopedasur/logopedasur/sesiones/models.py
+++ logopedasur/logopedasur/sesiones/models.py
@@ -8,6 +8,23 @@ from logopedasur.pacientes.models import Paciente, Tutor
 
 
 # Create your models here.
+@python_2_unicode_compatible
+class Sesiontipo(models.Model):
+    '''
+        Model for table Sesiontipo where we describe different
+        types of session and it's features
+    '''
+
+    class Meta:
+        verbose_name = _('sesion tipo')
+        verbose_name_plural = _('sesiones tipo')
+
+    nombre = models.CharField(_('nombre'), max_length=256,null=False)
+    facturable = models.BooleanField(_(u'Facturable'), default=True)
+    coste = models.DecimalField(_(u'coste'), max_digits=8, decimal_places=2, null=True)
+
+    def __str__(self):
+        return self.nombre + " " + str(self.facturable) + " " + str(self.coste)
 
 
 @python_2_unicode_compatible
@@ -18,20 +35,12 @@ class Sesion(models.Model):
         verbose_name = _('sesion')
         verbose_name_plural = _('sesiones')
 
-    TIPO_SESIONES_CHOICES = (
-        ('Entrevista', u'Entrevista'),
-        ('Sesion30', u'Sesion 30 minutos'),
-        ('Sesion45', u'Sesion 45 minutos'),
-    )
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(Sesiontipo, on_delete=models.SET_NULL, null=True)
     terapeutas = models.ManyToManyField(Terapeuta)
     fecha_sesion = models.DateField(_('Fecha de sesion'))
     hora_inicio = models.TimeField(_('Hora de inicio'))
     hora_fin = models.TimeField(_('Hora de finalizacion'))
-    tipo = models.CharField(_(u'tipo'), max_length=15,
-                           choices = TIPO_SESIONES_CHOICES,
-                           null=True)
-    facturable = models.BooleanField(_(u'Facturable'), default=True)
     observaciones = models.TextField(_('observaciones'), blank=True, null=True)
 
 
