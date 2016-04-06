9d2001d52a569cf5f4dd499bd3f35a3559133c6a
Sergio Sanchez <trukise@gmail.com>
2016-04-07 00:39:31 +0200

    Added sessions on terapeuta_details

--- logopedasur/logopedasur/terapeutas/views.py
+++ logopedasur/logopedasur/terapeutas/views.py
@@ -12,14 +12,41 @@ from django.template import RequestContext
 from logopedasur.terapeutas.models import Terapeuta
 from logopedasur.terapeutas.models import Especialidad
 from logopedasur.terapeutas.forms import TerapeutasForm, EspecialidadesForm
+from logopedasur.sesiones.models import Sesion
 
 # Create your views here.
 @login_required(login_url='/login/')
 def terapeutas_details(request, terapeutasitem_pk):
     terapeuta = Terapeuta.objects.get(pk=terapeutasitem_pk)
+    sesiones_terapeuta = Sesion.objects.filter(terapeutas__pk=terapeutasitem_pk)
+    paginator_sesiones = Paginator(sesiones_terapeuta, settings.SESIONES_PAGINATION_PAGES)
+    page = request.GET.get('page')
+    try:
+        sesiones_terapeuta = paginator_sesiones.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page
+        sesiones_terapeuta = paginator_sesiones.page(1)
+    except EmptyPage:
+        #if page is out of range , deliver last page of results
+        sesiones_terapeuta = paginator_sesiones.page(paginator_sesiones.num_pages)
+
+    if request.GET.get('tab') == 'sesiones':
+        tab_active = "sesiones"
+    elif request.GET.get('tab') == 'facturas':
+        tab_active = "facturas"
+    elif request.GET.get('tab') == 'informes':
+        tab_active = "informes"
+    else:
+        tab_active = "sesiones"
+
     return render_to_response("terapeutas/terapeutas_details.html",
-                              {'terapeuta': terapeuta},
-                              context_instance=RequestContext(request))
+                              {'username': request.user,
+                               'terapeuta': terapeuta,
+                               'sesiones_terapeuta': sesiones_terapeuta,
+                               'tab_active': tab_active},
+                               context_instance=RequestContext(request))
+
+
 
 @login_required(login_url='/login/')
 def terapeutas_list(request):
